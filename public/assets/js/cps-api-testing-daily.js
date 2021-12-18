e195305e27ab78abddd3cc5b2432e4a0220d7b31
Nathan Goldbaum <NathanGoldbaum@ctulocal1.org>
2021-12-18 06:39:22 -0600

    Convert data string to table

--- public/assets/js/cps-api-testing-daily.js
+++ public/assets/js/cps-api-testing-daily.js
@@ -18,8 +18,21 @@ function autorun() {
     let data = await fetch(fetchURL).then(
       response => response.json()
     );
+    const header = `
+<table>
+  <thead>
+    <tr><th scope="col">AdultCaseCount</th><th scope="col">ChildCaseCount</th><th scope="col">TotalPositiveCount</th><th scope="col">CloseContactsCount</th><th scope="col">CaseCountDate":"2021-12-16T00:00:00-06:00","TotalTestCount</th><th scope="col">TestedPositiveCount</th><th scope="col">InvalidTestCount</th><th scope="col">AdultTestCount</th><th scope="col">ChildTestCount</th></tr>
+  </thead>
+<tbody>
+`
+    let body = ``;
+    for (datum of data) {
+      const datumRow = `
+<tr><td>${datum.AdultCaseCount}</td><td>${datum.ChildCaseCount}</td><td>${datum.TotalPositiveCount}</td><td>${datum.CloseContactsCount}</td><td>${datum.CaseCountDate":"2021-12-16T00:00:00-06:00","TotalTestCount}</td><td>${datum.TestedPositiveCount}</td><td>${datum.InvalidTestCount}</td><td>${datum.AdultTestCount}</td><td>${datum.ChildTestCount}</td></tr>`;
+      body += datumRow;
+    }
+    const footer = "</tbody></table>";
     datastring = JSON.stringify(data);
-    output.innerHTML = datastring;
-
+    output.innerHTML = header + body + footer;
   });
 }
