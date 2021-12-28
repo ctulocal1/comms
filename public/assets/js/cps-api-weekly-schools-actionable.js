ebd6f0f1be0b6a1dbabcdce81eb5fc649d078558
Nathan Goldbaum <NathanGoldbaum@ctulocal1.org>
2021-12-27 22:30:21 -0600

    Complete Weekly Schools Actionable, pending input from Sarah.

--- public/assets/js/cps-api-weekly-schools-actionable.js
+++ public/assets/js/cps-api-weekly-schools-actionable.js
@@ -1,69 +1,56 @@
-function autorun() {
-  let commify = function (numberString) {
-    const number = parseInt(numberString);
-    const returnString = number.toLocaleString();
-    return returnString;
-  }
-  let convertDate = function (datestring) {
-    const date = new Date(datestring);
-    const dd = String(date.getDate()).padStart(2, '0');
-    const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
-    const yyyy = date.getFullYear();
+//
+// AutoRun is called when page the body is loaded.
 
-    const formatted = mm + '/' + dd + '/' + yyyy;
-    return formatted;
-  } 
+function autorun() {
   const lookup = document.getElementById("lookup"); 
-  const output = document.getElementById("dataOutput");
+  const dataDisplay = document.getElementById("dataOutput");
+  const progress = document.getElementById("queryOutput");
   lookup.addEventListener("click", async () => {
+    //
+    // Sending query
     const sentQuery = new Date();
-    output.innerText = "Query sent to CPS API at " + sentQuery + ".";
+    progress.innerText = "Query sent to CPS API at " + sentQuery + ".";
     const fetchURL = `https://api.cps.edu/health/cps/schoolweeklycovidactionable?startdate=2021-08-20`;
     let data = await fetch(fetchURL).then(
       response => response.json()
     );
+    //
+    // Upon query's return
     const responseTime = new Date() - sentQuery;
-    output.innerText += `
-Query response returned in  ${responseTime/1000} seconds. Processing data for display.
+    progress.innerText += `
+      Query response returned in ${responseTime/1000} seconds. 
+      Processing data for display.
 `;
-    const bySchool = unifySchoolData(data);
-    // output.innerText = JSON.stringify(data);
-    myClipboard(output);
+    // 
+    // Convert data and render to table
+    const bySchool = GroupDataBySchool(data);
+    dataTable = renderTable(bySchool);
+    dataDisplay.appendChild(dataTable);
+    progress.innerHTML +=`
+    <p>
+      <button class="btn" data-clipboard-target="#dataOutput" style="display:flex; align-items: center; justify-content:center;">
+        <img src="/assets/img/copied.svg" alt="" style="height: 1.6em; margin: 8px;"/>
+        <span>Copy table to clipboard</span>
+      </button>
+      <span id="copiedMsg"></span>
+    </p>
+    `
+
+    //
+    // Activate copy to clipboard
+    myClipboard(dataDisplay);
   });
+  return false;
 }
 
-function myClipboard (output) {
-  // ClipboardJS loaded with separate call in page body
-  var clipboard = new ClipboardJS('.btn');
-
-  clipboard.on('success', function (e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-
-    sayCopied(output);
-
-    e.clearSelection();
-  });
-
-  clipboard.on('error', function (e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
-  });
-}
-
-function sayCopied (output) {
-  output.style.background = "#222299";
-  let unhighlight = setTimeout( () => output.style.background = "transparent", 300);
-  const msgTarget = document.getElementById("copiedMsg");
-  msgTarget.innerText = "Table copied to clipboard";
-  let disappear = setTimeout( () => msgTarget.innerText = '', 1400);
-}
-function unifySchoolData (data) {
-  let schoolsData = [];
+// Data comes from the API as complete record for each week, including school.
+// This groups all the weeks for each school into one object as an item in the array.
+function GroupDataBySchool (data) {
+  let schoolsData = []; // This array is returned after processing.
   for (datum of data) {
     let schoolData = schoolsData.find(school => school.SchoolID === datum.SchoolID);
-    if (schoolData){
+
+    if (schoolData) { // If school is already in the array push this week's data.
       const week = {};
       week.End = datum.WeekEndDate;
       week.Start = datum.WeekStartDate;
@@ -71,7 +58,7 @@ function unifySchoolData (data) {
       week.Contacts = datum.CloseContactsCount;
       schoolData.Weeks.push(week);
 
-    } else {
+    } else { // If school is not found in the array, create the item.
 
       let school = {};
       school.SchoolID = datum.SchoolID;
@@ -86,8 +73,102 @@ function unifySchoolData (data) {
       school.Weeks.push(week);
       schoolsData.push(school);
       schoolData = schoolsData[schoolsData.length - 1];
-    }
-    //console.log(schoolData.Name,":",schoolData.Weeks[schoolData.Weeks.length - 1].End)
+    }// end of else
+
+  } // end of for loop over data
+  return schoolsData;
+  // console.log(schoolsData);
+}
+
+//
+// Creates the table that displays the data in final format.
+function renderTable(data) {
+  let table = document.createElement("table");
+  table.id="dataDisplay";
+  let thead = `
+    <thead>
+      <tr>
+        <th scope="col">School Name</th>
+        <th scope="col">Latest Week End Date</th>
+        <th scope="col">Latest Case Count</th>
+        <th scope="col">Latest Contact Count</th>
+        <th scope="col"># Weeks Reporting</th>
+        <th scope="col">YTD Case Count</th>
+        <th scope="col">YTD Contact Count</th>
+      </tr>
+    </thead>
+  `
+  table.innerHTML = thead;
+  let tbody = document.createElement("tbody");
+
+  for (datum of data) {
+    const weeks = datum.Weeks;
+    const allCases = datum.Weeks.map( item => item.Cases );
+    const allContacts = datum.Weeks.map( item => item.Contacts );
+    const sum = (item1, item2) => item1 + item2;
+    const YTDcases = allCases.reduce(sum);
+    const YTDcontacts = allContacts.reduce(sum);
+    weeks.sort( (a,b) => a.End - b.End);
+    // console.log(datum);
+    let tr = document.createElement("tr"); 
+    tr.innerHTML  = `<td>${datum.Name}</td>`;
+    tr.innerHTML += `<td>${convertDate(weeks[0].End)}</td>`
+    tr.innerHTML += `<td>${commify(weeks[0].Cases)}</td>`
+    tr.innerHTML += `<td>${commify(weeks[0].Contacts)}</td>`
+    tr.innerHTML += `<td>${commify(weeks.length)}</td>`
+    tr.innerHTML += `<td>${commify(YTDcases)}</td>`
+    tr.innerHTML += `<td>${commify(YTDcontacts)}</td>`
+    tbody.appendChild(tr);
   }
-  console.log(schoolsData);
+  table.appendChild(tbody);
+  return table;
+}
+
+//
+// Number formatting functions
+//
+function commify (numberString) {
+  const number = parseInt(numberString);
+  return number.toLocaleString();
+}
+
+function convertDate (datestring) {
+  const date = new Date(datestring);
+  const dd = String(date.getDate()).padStart(2, '0');
+  const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const yyyy = date.getFullYear();
+
+  const formatted = mm + '/' + dd + '/' + yyyy;
+  return formatted;
+} 
+
+//
+// Functions related to copying table to clipboard 
+//
+function myClipboard (dataDisplay) {
+  // ClipboardJS loaded with separate call in page body
+  var clipboard = new ClipboardJS('.btn');
+
+  clipboard.on('success', function (e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    sayCopied(dataDisplay);
+
+    e.clearSelection();
+  });
+
+  clipboard.on('error', function (e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+  });
+}
+
+function sayCopied (dataDisplay) {
+  dataDisplay.style.background = "#222299";
+  let unhighlight = setTimeout( () => dataDisplay.style.background = "transparent", 300);
+  const msgTarget = document.getElementById("copiedMsg");
+  msgTarget.innerText = "Table copied to clipboard";
+  let disappear = setTimeout( () => msgTarget.innerText = '', 1400);
 }
