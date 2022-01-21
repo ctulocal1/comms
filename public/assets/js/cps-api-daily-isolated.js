c9d8c0a3e55666b8bc65811b8e1764f9a1ca22db
Nathan Goldbaum <NathanGoldbaum@ctulocal1.org>
2022-01-20 22:01:43 -0600

    Make Daily Quarantine page.

--- public/assets/js/cps-api-daily-isolated.js
+++ public/assets/js/cps-api-daily-isolated.js
@@ -4,24 +4,32 @@
 function autorun() {
   const lookup = document.getElementById("lookup"); 
   const dataDisplay = document.getElementById("dataOutput");
-  const buttons = document.querySelector("#buttons .btn");
-  const weekPicker = document.getElementById("weeks");
+  const buttons = document.querySelector(".btn");
+  const datePicker = document.getElementById("date");
+  const dateLabel = document.querySelector("[for='date']");
   const progress = document.getElementById("queryOutput");
+  datePicker.style.visibility = "hidden";
+  dateLabel.style.visibility = "hidden";
+  buttons.style.visibility = "hidden";
   let bySchool = [];
   lookup.addEventListener("click", async () => {
+    let populations = await fetch("/assets/data/studentcounts.json" )
+    .then(response => response.json())
     //
     // Sending query
-    dataDisplay.innerHTML = "";
+    dataDisplay.innerHTML = "<p>The data table will appear here after the query response is returned and processed.</p>";
     const sentQuery = new Date();
-    progress.innerText = "Query sent to CPS API at " + sentQuery + ". Response can take up to a minute or longer, depending on network traffic.\nAwaiting Response.";
-    const fetchURL = `https://api.cps.edu/health/cps/schoolweeklycovidactionable?startdate=2021-08-20`;
+    progress.innerText = "Query sent to CPS API at " + sentQuery + ". Response can take a full minute or longer, depending on network traffic.\nAwaiting Response.";
+    const fetchURL = `https://api.cps.edu/health/cps/SchoolNoAdmittanceSummary`;
     let counting = setInterval (function() { progress.innerText += "." }, 1000);
-    buttons.style.visibility = "visible";
     let data = await fetch(fetchURL).then(
       response => response.json()
     );
     //
     // Upon query's return
+    datePicker.style.visibility = "visible";
+    dateLabel.style.visibility = "visible";
+    buttons.style.visibility = "visible";
     clearInterval(counting);
     const responseTime = new Date() - sentQuery;
     progress.innerText += `
@@ -30,23 +38,22 @@ function autorun() {
 `;
     // 
     // Convert data and render to table
-    bySchool = GroupDataBySchool(data,weekPicker);
-    let selectedDate = weekPicker.options[weekPicker.selectedIndex];
+    bySchool = GroupDataBySchool(data,datePicker,populations);
+    let selectedDate = datePicker.options[datePicker.selectedIndex];
     let selectedDateValue = selectedDate.value;
     let selectedDateText = selectedDate.text;
+    dataDisplay.innerHTML = `<h3>Case Counts for the week ending ${selectedDateText}</h3>`;
     let ordered = orderRows(selectedDateValue,bySchool)
     dataTable = renderTable(ordered);
-    dataDisplay.innerHTML = `<h3>Case Counts for the week ending ${selectedDateText}</h3>`;
     dataDisplay.appendChild(dataTable);
-    buttons.style.visibility = "visible";
 
     //
     // Activate copy to clipboard
     myClipboard(dataDisplay);
   });
   lookup.click();
-  weekPicker.addEventListener ( "change", () => {
-    const selectedDate = weekPicker.options[weekPicker.selectedIndex];
+  datePicker.addEventListener ( "change", () => {
+    const selectedDate = datePicker.options[datePicker.selectedIndex];
     const selectedDateValue = selectedDate.value;
     const selectedDateText =  selectedDate.text;
     let ordered = orderRows(selectedDateValue,bySchool);
@@ -61,62 +68,53 @@ function orderRows (selected,schools) {
   let rows = [];
   for (school of schools) {
     const row = {
-      name: school.Name,
-      wkCases: 0,
-      wkContacts: 0,
-      wksReporting: 0,
-      YTDcases: 0,
-      YTDcontacts:0
+      name: school.name,
+      persons: 0,
+      population: 0,
+      percent: 0
     }
-    let matchWeek = school.Weeks.find( week => week.End === selected );
-    if (matchWeek) {
-      row.wkCases = matchWeek.Cases;
-      row.wkContacts = matchWeek.Contacts;
+    let matchDate = school.dates.find( date => date.date === selected );
+    if (matchDate) {
+      row.persons = matchDate.persons;
+      row.population = school.population;
+      row.percent = Math.round( 1000 * row.persons / row.population )/10;
+      rows.push(row)
     }
-    for (week of school.Weeks) {
-      if (week.End === selected || week.End < selected) {
-        row.YTDcases += week.Cases;
-        row.YTDcontacts += week.Contacts;
-        row.wksReporting++;
-      }
-    }
-    rows.push(row)
   }
-  rows.sort( (a,b) => b.wkCases - a.wkCases);
+  console.log(rows);
+  rows.sort( (a,b) => b.percent - a.percent);
   return rows;
 }
 
 // Data comes from the API as complete record for each week, including school.
 // This groups all the weeks for each school into one object as an item in the array.
-function GroupDataBySchool (data,weekPicker) {
+function GroupDataBySchool (data,datePicker,populations) {
   let dateSet = new Set();
   let schoolsData = []; // This array is returned after processing.
   for (datum of data) {
-    let schoolData = schoolsData.find(school => school.SchoolID === datum.SchoolID);
+    dateSet.add(datum.RecordDate);
 
-    dateSet.add(datum.WeekEndDate);
-
-    if (schoolData) { // If school is already in the array push this week's data.
-      const week = {};
-      week.End = datum.WeekEndDate;
-      week.Start = datum.WeekStartDate;
-      week.Cases = datum.TotalCaseCount;
-      week.Contacts = datum.CloseContactsCount;
-      schoolData.Weeks.push(week);
+    let schoolData = schoolsData.find(school => school.id === datum.SchoolID);
+    
+    if (schoolData) { // If school is already in the array push this report's data.
+      const report = {};
+      report.date = datum.RecordDate;
+      report.persons = parseInt( datum.PersonTotal );
+      schoolData.dates.push(report);
 
     } else { // If school is not found in the array, create the item.
-
-      let school = {};
-      school.SchoolID = datum.SchoolID;
-      school.Name = datum.SchoolShortName;
-      school.Network = datum.NetworkName;
-      school.Weeks = [];
-      const week = {};
-      week.Start = datum.WeekStartDate;
-      week.End = datum.WeekEndDate;
-      week.Cases = datum.TotalCaseCount;
-      week.Contacts = datum.CloseContactsCount;
-      school.Weeks.push(week);
+      const school = {};
+      school.id = datum.SchoolID;
+      school.name = datum.SchoolShortName;
+      const matchedSchool = populations.find(row => row.id.toString() === school.id);
+      if (matchedSchool) {
+        school.population = matchedSchool.students;
+      }
+      const report = {};
+      report.date = datum.RecordDate;
+      report.persons = parseInt( datum.PersonTotal );
+      school.dates = [];
+      school.dates.push(report);
       schoolsData.push(school);
       schoolData = schoolsData[schoolsData.length - 1];
     }// end of else
@@ -128,40 +126,37 @@ function GroupDataBySchool (data,weekPicker) {
     for (date of dateArray) {
       const dateval = new Date(date);
       const dateString =  new Intl.DateTimeFormat('en-US', {month:"short",day:"2-digit"}).format(dateval).toUpperCase();
-      weekPicker.innerHTML += `<option value="${date}">${dateString}</option>`;
+      datePicker.innerHTML += `<option value="${date}">${dateString}</option>`;
     }
+  console.log(schoolsData);
   return schoolsData;
-  // console.log(schoolsData);
 }
 
 //
 // Creates the table that displays the data in final format.
-function renderTable(data) {
+function renderTable(rows) {
   let table = document.createElement("table");
   table.id="dataDisplay";
   let thead = `
     <thead>
       <tr>
-        <th scope="col">School Name</th>
-        <th scope="col">Latest Case Count</th>
-        <th scope="col">Latest Contact Count</th>
-        <th scope="col"># Weeks Reporting</th>
-        <th scope="col">YTD Case Count</th>
-        <th scope="col">YTD Contact Count</th>
+        <th scope="col">Name</th>
+        <th scope="col">Quarantined</th>
+        <th scope="col">Population</th>
+        <th scope="col">Percent Quarantined</th>
       </tr>
     </thead>
   `
   table.innerHTML = thead;
   let tbody = document.createElement("tbody");
 
-  for (datum of data) {
+  for (row of rows) {
     let tr = document.createElement("tr"); 
-    tr.innerHTML  = `<td>${datum.name}</td>`;
-    tr.innerHTML += `<td>${commify(datum.wkCases)}</td>`
-    tr.innerHTML += `<td>${commify(datum.wkContacts)}</td>`
-    tr.innerHTML += `<td>${commify(datum.wksReporting)}</td>`
-    tr.innerHTML += `<td>${commify(datum.YTDcases)}</td>`
-    tr.innerHTML += `<td>${commify(datum.YTDcontacts)}</td>`
+    const percent = row.percent.toFixed(1);
+    tr.innerHTML  = `<td>${row.name}</td>`;
+    tr.innerHTML += `<td>${commify( row.persons )}</td>`
+    tr.innerHTML += `<td>${commify( row.population )}</td>`
+    tr.innerHTML += `<td>${percent}%</td>`
     tbody.appendChild(tr);
   }
   table.appendChild(tbody);
