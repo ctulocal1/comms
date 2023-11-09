4120589fd9be81246f02e5d390ac273121b35b3e
ctulocal1 <NathanGoldbaum@ctulocal1.org>
2023-11-09 16:40:58 -0600

    Update sports23.js to allow all origins in top const

--- netlify/functions/sports23.js
+++ netlify/functions/sports23.js
@@ -1,5 +1,5 @@
 const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': 'https://www.ctulocal1.org',
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers':
     'Origin, X-Requested-With, Content-Type, Accept',
 }
